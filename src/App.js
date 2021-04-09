8474001975bd46f3a925235bd43d5db7f890e0da
Karen <devkarenc@gmail.com>
2021-04-09 16:43:51 -0400

    Add logic to update the cards clicked and checking if game is over

--- src/App.js
+++ src/App.js
@@ -4,12 +4,36 @@ import Header from "./components/Header";
 import Scoreboard from "./components/Scoreboard";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
-
 import characters from "./components/characters";
 
 const App = () => {
-  // randomize card display upon card click
+  const [gameOver, setGameOver] = useState(() => false);
+  const [cardsClicked, setCardsClicked] = useState(() => []);
+
+  const updateCardsClicked = (newCard) => {
+    setCardsClicked((cardsClicked) => {
+      return cardsClicked.concat([newCard]);
+    });
+  };
+
+  // only run when the cardsClicked array has changed (componentDidUpdate)
+  useEffect(() => {
+    // logic to check if there are duplicate cards, meaning game over
+    const updateGameOver = () => {
+      if (cardsClicked.length !== new Set(cardsClicked).size) {
+        setGameOver(true);
+      }
+    };
+    updateGameOver();
+  }, [cardsClicked]);
+
+  // randomize card display upon card click (componentDidMount - useEffect hook)
   const randomizeCardDisplay = () => {};
+
+  // will run when the component is updated and after the initial render
+  // useEffect(() => {
+  //   randomizeCardDisplay();
+  // });
 
   return (
     <React.Fragment>
@@ -17,7 +41,17 @@ const App = () => {
       <Scoreboard />
       <div className="cards-container">
         {characters.map((char) => {
-          return <Card key={char.name} name={char.name} src={char.src} />;
+          return (
+            <Card
+              key={char.name}
+              name={char.name}
+              src={char.src}
+              onClick={() => {
+                updateCardsClicked(char.name);
+                // randomizeCardDisplay();
+              }}
+            />
+          );
         })}
       </div>
       <Footer />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../styles/Card.css";
 
-const Card = ({ name, src }) => {
+const Card = ({ name, src, onClick }) => {
   return (
-    <div className="card-container">
+    <div className="card-container" onClick={onClick}>
       <img className="char-image" src={src} alt={name} />
       <p className="char-name">{name}</p>
     </div>
